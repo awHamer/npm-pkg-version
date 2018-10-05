b5198814206519a7e55d42fbd393710f916c7e5f
Dmitry Kvokov <kvokovdl@gmail.com>
2018-10-05 21:12:26 +0300

    Added eslint, improved code style

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
 #!/usr/bin/env node
-'use strict';
-var childProcess = require('child_process');
-var fs = require('fs');
-var argv = process.argv.slice(2);
-var styles = {
-  // red: "\x1b[31m",
-  red: "\x1b[35m",
-  green: "\x1b[32m",
-  underscore: "\x1b[4m",
-  reset: "\x1b[0m",
-};
-var localModulesPath = process.cwd() + '/node_modules';
-var isGlobal = argv[0] === 'global' && argv.length > 1;
 
-if (argv.includes('--help')) {
-  getHelp();
-}
-if (argv.includes('--version') || argv.includes('-v')) {
-  printVersion();
-}
+const fs = require('fs');
+const childProcess = require('child_process');
+const { version: selfVersion } = require('./package.json');
+
+/* eslint-disable no-console */
+
+const argv = process.argv.slice(2);
+const localModulesPath = `${process.cwd()}/node_modules`;
+const isGlobal = argv[0] === 'global' && argv.length > 1;
+const styles = {
+  red: '\x1b[35m',
+  green: '\x1b[32m',
+  underscore: '\x1b[4m',
+  reset: '\x1b[0m',
+};
 
 function getNodeModulesPath() {
-  return isGlobal ? childProcess.execSync('npm root -g').toString().trim("\n") : localModulesPath;
+  return isGlobal ? childProcess.execSync('npm root -g').toString().trim() : localModulesPath;
 }
 
 function printVersion() {
-  console.log('v' + require('./package.json').version);
+  console.log(`v${selfVersion}`);
   process.exit(0);
 }
 
 function getHelp() {
   console.log('');
-  console.log('Usage: ');
-  console.log('$ npmver <package_name> -- print locally installed package version');
-  console.log('$ npmver global <package_name> -- print globally installed package version');
+  console.log('NPM package version checker');
   console.log('');
-  console.log('npmver@' + require('./package.json').version);
+  console.log('Usage: ');
+  console.log(' $ npmver <package_name> -- print locally installed package version');
+  console.log(' $ npmver global <package_name> -- print globally installed package version');
+  console.log('');
+  console.log(`npmver@${selfVersion}`);
+  console.log('');
   process.exit(0);
 }
 
@@ -55,12 +54,12 @@ function getVersion(packageName) {
   }
 
   try {
-    var pkg = require(getNodeModulesPath() + '/' + packageName + '/package.json');
+    const pkg = require(`${getNodeModulesPath()}/packageName/package.json`); // eslint-disable-line global-require, import/no-dynamic-require
     console.log(' -> Package Name:', styles.green, packageName, styles.reset, isGlobal ? '(global)' : '');
-    console.log(styles.underscore, "-> Package version:", styles.green, pkg.version);
+    console.log(styles.underscore, '-> Package version:', styles.green, pkg.version);
   } catch (e) {
     if (e.code === 'MODULE_NOT_FOUND') {
-      error('Module "' + packageName + '" was not found in your node_modules folder.');
+      error(`Module "${packageName}" was not found in your node_modules folder.`);
       if (!isGlobal) {
         console.log(' Looking for a global package version? add "global" flag before <package_name>');
       }
@@ -68,6 +67,13 @@ function getVersion(packageName) {
     }
     throw e;
   }
+}
+
+if (argv.includes('--help') || argv.includes('-h')) {
+  getHelp();
+}
+if (argv.includes('--version') || argv.includes('-v')) {
+  printVersion();
 }
 
 getVersion(argv[isGlobal ? 1 : 0]);
